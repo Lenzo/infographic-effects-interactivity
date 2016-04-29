7d419faad10961448fce168704abe4411a55e56a
Lenzo <lenbunce@Lens-MacBook-Pro.local>
2016-04-29 17:30:02 -0400

    fix jquery syntax

--- js/main.js
+++ js/main.js
@@ -1,41 +1,36 @@
+function countBrainType (elementID, buttonValue) {
 
+  var resultDivId = '#' + elementID;
 
-$(document).ready(function(){
+  var trueCount = 0;
+  var falseCount = 0;
 
-	var nextQuestionDivId;
-
-	$("input").on("click", function() {
-		$(this).closest("div").hide();
-	    nextQuestionDivId = '#' + ($(this).closest("div")).next("div").attr("id");
-        $(nextQuestionDivId).removeClass("hide-div");
-		countBrainType(($(this).closest("div")).next("div").attr("id"), $(this).attr("value"));
-	});
-});
-
-//calculate the score 
-function countBrainType(elementID,buttonValue) {
-
-	var resultDivId = '#' + elementID;
-
-
-	var trueCount = 0;
-	var falseCount = 0;
-
-  if (buttonValue === "true") {
+  if (buttonValue === 'true') {
     trueCount++;
   } else {
     falseCount++;
   }
-   if (elementID === "demo"){
-	 if (trueCount === falseCount) {
-	 	$(resultDivId).children("p").text("You are ambidextrous brained");
-	 }
-
-	 else if (trueCount > falseCount) {
-	      $(resultDivId).children("p").text("You are left brained!");
-	 }
-	 else {
-		 $(resultDivId).children("p").text("You are right brained!");
-	 }
-   }
+  if (elementID === 'demo') {
+    if (trueCount === falseCount) {
+      $(resultDivId).children('p').text('You are ambidextrous brained');
+    } else if (trueCount > falseCount) {
+      $(resultDivId).children('p').text('You are left brained!');
+    } else {
+      $(resultDivId).children('p').text('You are right brained!');
+    }
+  }
 }
+
+$(document).ready(function () {
+
+  var nextQuestionDivId;
+
+  $('input').on('click', function () {
+    $(this).closest('div').hide();
+    nextQuestionDivId = '#' + ($(this).closest('div')).next('div')
+    .attr('id');
+    $(nextQuestionDivId).removeClass('hide-div');
+    countBrainType(($(this).closest('div')).next('div')
+      .attr('id'), $(this).attr('value'));
+  });
+});
